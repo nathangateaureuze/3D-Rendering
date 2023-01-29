3d3fabcdfde5b2b0833b1495a673e3d97d079cf3
nathangateaureuze <92450570+nathangateaureuze@users.noreply.github.com>
2023-01-29 21:46:45 +0100

    rotation axe y

--- main.c
+++ main.c
@@ -6,10 +6,14 @@
 #include <SDL2/SDL.h>
 #undef main
 
-#define WINDOW_WIDTH 600
+#undef M_PI
+#include <math.h>
+
+#define WINDOW_WIDTH 400
 
 
-void line(SDL_Renderer *renderer, int x1, int y1, int x2, int y2) {
+void line(SDL_Renderer *renderer, int x1, int y1, int x2, int y2) 
+{
     int dx = abs(x2 - x1);
     int dy = abs(y2 - y1);
     int sx = x1 < x2 ? 1 : -1;
@@ -19,16 +23,19 @@ void line(SDL_Renderer *renderer, int x1, int y1, int x2, int y2) {
 
     SDL_RenderDrawPoint(renderer, x1, y1);
 
-    while (x1 != x2 || y1 != y2) {
-        SDL_RenderDrawPoint(renderer, x1, y1);
+    while (x1 != x2 || y1 != y2) 
+    {
+        SDL_RenderDrawPoint(renderer, (WINDOW_WIDTH/2)+x1, (WINDOW_WIDTH/2)+y1);
         error2 = 2 * error;
         
-        if (error2 > -dy) {
+        if (error2 > -dy) 
+        {
             error -= dy;
             x1 += sx;
         }
 
-        if (error2 < dx) {
+        if (error2 < dx) 
+        {
             error += dx;
             y1 += sy;
         }
@@ -36,7 +43,15 @@ void line(SDL_Renderer *renderer, int x1, int y1, int x2, int y2) {
     SDL_RenderPresent(renderer);
 }
 
-void shape(SDL_Renderer *renderer, int points[3][3], int relations[3][2], int relationssize)
+void rotation(int points[8][3], double rotation, int pointssize)
+{
+    for (int i = 0; i < pointssize; ++i)
+    {
+        points[i][0] = round(points[i][0] * sin(rotation) + points[i][2] * cos(rotation));
+    }
+}
+
+void shape(SDL_Renderer *renderer, int points[8][3], int relations[12][2], int relationssize)
 {
     for (int i = 0; i < relationssize; ++i)
     {
@@ -50,6 +65,7 @@ int main(void)
     SDL_Renderer *renderer1;
     SDL_Window *window;
     int i;
+    double rot = 0;
 
     SDL_Init(SDL_INIT_VIDEO);
     SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_WIDTH, 0, &window, &renderer1);
@@ -58,8 +74,8 @@ int main(void)
 
     while (1) 
     {
-        int points1[3][3] = {{20,20,20},{360,60,40},{540,410,10}};
-        int relations1[3][2] = {{0,1},{1,2},{0,2}};
+        int points1[8][3] = {{-100,-100,100},{100,-100,100},{-100,100,100},{100,100,100},{-100,-100,-100},{100,-100,-100},{-100,100,-100},{100,100,-100}};
+        int relations1[12][2] = {{0,1},{0,2},{1,3},{2,3},{0,4},{2,6},{3,7},{1,5},{7,5},{4,5},{6,7},{4,6}};
 
         if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
         {
@@ -68,6 +84,10 @@ int main(void)
         else if(event.type == SDL_KEYDOWN) 
         {
             const char *key = SDL_GetKeyName(event.key.keysym.sym);
+            if(strcmp(key, "Z") == 0) 
+            {
+                break;
+            } 
             if(strcmp(key, "Q") == 0) 
             {
                 SDL_SetRenderDrawColor(renderer1, 200, 200, 200, 255);
@@ -88,9 +108,11 @@ int main(void)
             {
                 SDL_SetRenderDrawColor(renderer1, 0, 0, 0, 0);
                 SDL_RenderClear(renderer1);
-
                 SDL_SetRenderDrawColor(renderer1, 255, 0, 0, 255);
+
                 //line(renderer1, 120, 161, 55, 32);
+                rotation(points1, rot, sizeof(points1)/sizeof(points1[0]));
+                rot += 0.15;
                 shape(renderer1, points1, relations1, sizeof(relations1)/sizeof(relations1[0]));
             }             
         }
