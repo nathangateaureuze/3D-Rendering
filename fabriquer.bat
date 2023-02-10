a7dc8c2b75b92e1f52a790adf8a13350f16cf4c7
nathangateaureuze <92450570+nathangateaureuze@users.noreply.github.com>
2023-02-10 16:07:07 +0100

    roation xyz + essai souris

---
+++ fabriquer.bat
@@ -0,0 +1,3 @@
+set PATH="C:\Users\GAME2\Desktop\tcc";%PATH%
+tcc main.c -lSDL2
+main
--- main.c
+++ main.c
@@ -43,11 +43,23 @@ void line(SDL_Renderer *renderer, int x1, int y1, int x2, int y2)
     SDL_RenderPresent(renderer);
 }
 
-void rotation(int points[8][3], double rotation, int pointssize)
+void rotation(int points[8][3], double rotationx, double rotationy, int pointssize)
 {
     for (int i = 0; i < pointssize; ++i)
     {
-        points[i][0] = round(points[i][0] * sin(rotation) + points[i][2] * cos(rotation));
+        int x = points[i][0];
+        int y = points[i][1];
+        int z = points[i][2];
+		
+		//Rotation axe X
+        //points[i][1] = round(y * cos(rotationx) + z * sin(rotationx));
+		
+		//Rotation axe Y
+		points[i][0] = round(x * -sin(rotationy) + z * cos(rotationy));
+		
+		//Rotation axe Z
+        //points[i][0] = round(x * cos(rotation) + y * sin(rotation));
+        //points[i][1] = round(x * -sin(rotation) + y * cos(rotation));
     }
 }
 
@@ -65,18 +77,35 @@ int main(void)
     SDL_Renderer *renderer1;
     SDL_Window *window;
     int i;
-    double rot = 0;
+    int time = 0;
 
     SDL_Init(SDL_INIT_VIDEO);
     SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_WIDTH, 0, &window, &renderer1);
     SDL_SetRenderDrawColor(renderer1, 0, 0, 0, 0);
     SDL_RenderClear(renderer1);
 
+    if(SDL_SetRelativeMouseMode(SDL_TRUE) != 0)
+    {
+        SDL_GetError();
+        SDL_DestroyRenderer(renderer1);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 0;
+    }
+	
+
+    int mousepositionx;
+    int mousepositiony;
+
+    double rx = 0;
+    double ry = 0;
+    double rz = 0;
+
     while (1) 
     {
-        int points1[8][3] = {{-100,-100,100},{100,-100,100},{-100,100,100},{100,100,100},{-100,-100,-100},{100,-100,-100},{-100,100,-100},{100,100,-100}};
-        int relations1[12][2] = {{0,1},{0,2},{1,3},{2,3},{0,4},{2,6},{3,7},{1,5},{7,5},{4,5},{6,7},{4,6}};
-
+		int points1[8][3] = {{-100,-100,100},{100,-100,100},{-100,100,100},{100,100,100},{-100,-100,-100},{100,-100,-100},{-100,100,-100},{100,100,-100}};
+		int relations1[12][2] = {{0,1},{0,2},{1,3},{2,3},{0,4},{2,6},{3,7},{1,5},{7,5},{4,5},{6,7},{4,6}};
+		
         if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
         {
             break;
@@ -87,22 +116,6 @@ int main(void)
             if(strcmp(key, "Z") == 0) 
             {
                 break;
-            } 
-            if(strcmp(key, "Q") == 0) 
-            {
-                SDL_SetRenderDrawColor(renderer1, 200, 200, 200, 255);
-            } 
-            else if(strcmp(key, "D") == 0) 
-            {
-                SDL_SetRenderDrawColor(renderer1, 0, 0, 0, 0);
-                SDL_RenderClear(renderer1);
-
-                SDL_SetRenderDrawColor(renderer1, 255, 0, 0, 255);
-                for (i = 0; i < WINDOW_WIDTH; ++i)
-                {
-                    SDL_RenderDrawPoint(renderer1, i, i);
-                }
-                SDL_RenderPresent(renderer1);
             }
             else if(strcmp(key, "A") == 0) 
             {
@@ -110,11 +123,44 @@ int main(void)
                 SDL_RenderClear(renderer1);
                 SDL_SetRenderDrawColor(renderer1, 255, 0, 0, 255);
 
-                //line(renderer1, 120, 161, 55, 32);
-                rotation(points1, rot, sizeof(points1)/sizeof(points1[0]));
-                rot += 0.15;
+				rx += 0.15;
+                rotation(points1, rx, ry, sizeof(points1)/sizeof(points1[0]));
                 shape(renderer1, points1, relations1, sizeof(relations1)/sizeof(relations1[0]));
             }             
+        }
+        else if(event.type == SDL_MOUSEMOTION && time == 1) 
+        {
+            SDL_SetRenderDrawColor(renderer1, 0, 0, 0, 0);
+            SDL_RenderClear(renderer1);
+            SDL_SetRenderDrawColor(renderer1, 255, 0, 0, 255);
+
+            SDL_GetRelativeMouseState(&mousepositionx, &mousepositiony);
+            if (mousepositionx > 20)
+            {
+                ry += 0.15;
+            }
+            else if (mousepositionx < -20)
+            {
+                ry -= 0.15;
+            }
+            if (mousepositiony > 20)
+            {
+                rx += 0.15;
+            }
+            else if (mousepositiony < -20)
+            {
+                rx -= 0.15;
+            }
+            rotation(points1, rx, ry, sizeof(points1)/sizeof(points1[0]));
+            shape(renderer1, points1, relations1, sizeof(relations1)/sizeof(relations1[0]));
+        }
+        if (time == 1)
+        {
+            time = 0;
+        }
+        else
+        {
+            time += 1;
         }
     }
     SDL_DestroyRenderer(renderer1);
